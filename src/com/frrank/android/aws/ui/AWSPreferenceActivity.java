b530aabfb72d49ae2709ee9f85ba3b25f3294970
Frrank <avveustudios@yahoo.com>
2016-05-01 07:16:15 -0700

    pushing aws application to git

---
+++ src/com/frrank/android/aws/ui/AWSPreferenceActivity.java
@@ -0,0 +1,37 @@
+package com.frrank.android.aws.ui;
+
+import com.frrank.android.aws.R;
+import com.frrank.android.aws.app.AppLog;
+import com.frrank.android.aws.constants.Constants;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+
+public class AWSPreferenceActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.preference);
+		
+		setPreferenceChangeListener(Constants.PREF_DIRECTORY_LISTING, onPreferenceChange);
+		setPreferenceChangeListener(Constants.PREF_DIRECTORY, onPreferenceChange);
+	}
+	
+	private void setPreferenceChangeListener(String key, Preference.OnPreferenceChangeListener listener){
+		Preference preference = findPreference(key);
+		
+		preference.setOnPreferenceChangeListener(onPreferenceChange);
+	}
+	
+	private Preference.OnPreferenceChangeListener onPreferenceChange = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+			AppLog.logString("Preference Change: " + preference.getKey() + ", " + newValue.toString());
+			
+			return true;
+		}
+	};
+}
